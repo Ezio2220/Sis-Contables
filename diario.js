9150c2d1ef9bcdeb30fb8c3e09149764f349fb5f
Ezio2220 <fjosue447@gmail.com>
2019-10-20 18:20:04 -0600

    agrega funciones para el total

--- diario.js
+++ diario.js
@@ -13,17 +13,33 @@ function agregar(){
 
     fecha=document.getElementById("fecha").value;
     var cont = document.getElementById("contenido");
-    
+
+    //se toman los valores descritos dentro de la etiqueta con los id: debet, y habert que corresponden a las celdas donde estan estos totales
+    var t1 = document.getElementById("debet").innerHTML;
+    var t2 = document.getElementById("habert").innerHTML;
+    //pero como estos tienen el signo $ hay que separar los numeros del signo $ para poder sumarlos o restarlos
+    var nt1 = parseFloat(t1.substring(1));//t1.substring() toma una parte de una cadena esta vez empezando del caracter 1 ya que el caracter 0 es el signo "$"
+    var nt2 = parseFloat(t2.substring(1));//y ademas se debe convertir lo que queda en un numero, por eso usamos parseFloat() para convertirlo en numero con decimales
+
     var resp  = cont.innerHTML ;
     if(tipo=="debe"){
         resp+= "<tr> <td>"+ x + "</td>";
         resp+= "<td>$" + valor + " </td> <td></td> ";
+        nt1 += parseFloat(valor);//cuando es el debe el que se agrego se suma al total numerico del debe
+        
     }else{
         resp += "<tr> <td style='text-align: center;' > "+ x + "</td>";
         resp+= "<td></td> <td>$" + valor + " </td> ";
+        nt2+=parseFloat(valor);//si no es el debe entonces se suma en el total del haber
     }
+//se inserta el resultado junto con el signo $ dentro de la etiqueta con el id debet, y habert (igual que arriba pero esta vez escribimos dentro en lugar de tomarlo)
+    document.getElementById("debet").innerHTML = "$"+ nt1;
+    document.getElementById("habert").innerHTML = "$"+ nt2;
+
     cont.innerHTML = resp;
 }
 function guardar(){
     document.getElementById("contenido").innerHTML="";
+    document.getElementById("debet").innerHTML = "$0.00";
+    document.getElementById("habert").innerHTML = "$0.00";
 }
